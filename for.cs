4b30f51d9a15f0ccb27e3b631706db25c1386618
Thaynara <104026459+thaynara15@users.noreply.github.com>
2023-07-07 15:08:10 -0300

    Add files via upload

---
+++ for.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace @for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] vetDiaSemana = new string[] {
+            "Segunda-feira",
+            "Terça-feira",
+            "Quarta-feira",
+            "Quinta-feira",
+            "Sexta-feira",
+            "Sábado",   
+            "Domingo"
+
+            };
+
+            for (int i = 0; i < vetDiaSemana.Length; i++)
+            {
+                Console.WriteLine(vetDiaSemana[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
