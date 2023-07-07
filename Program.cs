6d9b699338523282eababdae812b55d212db28b1
Thaynara <104026459+thaynara15@users.noreply.github.com>
2023-07-07 15:06:52 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -2,93 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading;
 
-namespace While
+namespace @for
 {
     class Program
     {
         static void Main(string[] args)
         {
-            ////While = enquanto
 
-            //While (Condição) {
-            //    [cod]  
-            //}
+            string[] vetDiaSemana = new string[] {
+            "Segunda-feira",
+            "Terça-feira",
+            "Quarta-feira",
+            "Quinta-feira",
+            "Sexta-feira",
+            "Sábado",   
+            "Domingo"
 
-            //int pos = 0;
+            };
 
-            //while (pos < 10000) {
-            //    Console.WriteLine(pos);
-
-            //    pos = (pos + 1);
-            //}
-
-            //Console.ReadKey();
-
-            //string[] vetDiaSemana = new string[] {
-            //"Segunda-feira",
-            //"Terça-feira",
-            //"Quarta-feira",
-            //"Quinta-feira",
-            //"Sexta-feira",
-            //"Sábado",
-            //"Domingo",
-            //"Feriado"
-            //};
-
-            //int pos = 0;
-            //while (pos < vetDiaSemana.Length)
-            //{
-
-            //    Console.WriteLine(vetDiaSemana[pos]);
-            //    pos++;
-
-            //}
-
-            Console.WriteLine("MENU");
-            Console.WriteLine("1 - Olá, bom dia");
-            Console.WriteLine("2 - Olá, boa tarde");
-            Console.WriteLine("3 - Olá, boa noite");
-            Console.WriteLine("4 - Olá, sair");
-
-            int opcao;
-            int.TryParse(Console.ReadLine(), out opcao);
-
-            while (opcao != 4) 
+            for (int i = 0; i < vetDiaSemana.Length; i++)
             {
-                switch (opcao)
-                {
-                    case 1:
-                        Console.ForegroundColor = ConsoleColor.Blue;
-                        Console.WriteLine("Olá, bom dia");
-                        break;
-                    case 2:
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                        Console.WriteLine("Olá, boa tarde");
-                            break;
-                    case 3:
-                        Console.ForegroundColor = ConsoleColor.DarkBlue;   
-                        Console.WriteLine("Olá, boa noite");
-                            break;
-                    default:
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Valor inválido");
-                            break;
-
-                }
-
-                Console.WriteLine("Precione uma tecla para continuar");
-                Console.ReadKey();
-                Console.Clear(); // limpa os textos console
-
-                Console.WriteLine("MENU");
-                Console.WriteLine("1 - Olá, bom dia");
-                Console.WriteLine("2 - Olá, boa tarde");
-                Console.WriteLine("3 - Olá, boa noite");
-                Console.WriteLine("4 - Olá, sair");
-
-                int.TryParse(Console.ReadLine(), out opcao);
+                Console.WriteLine(vetDiaSemana[i]);
             }
 
             Console.ReadKey();
